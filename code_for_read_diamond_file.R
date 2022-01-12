35fe17739c7c31bb4049c46a5c54d2719dc6568c
Vinícius Castro Santos <96088485+vini8cs@users.noreply.github.com>
2022-01-12 12:37:56 -0300

    Update code_for_read_diamond_file.R

--- code_for_read_diamond_file.R
+++ code_for_read_diamond_file.R
@@ -2,13 +2,11 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
-# test if there is at least one argument: if not, return an error
+# test if there is at least one argument. If not, return an error
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)==1) {
-  # default output file
-  args[2] = "out.txt"
-  #args[3] = "out2.txt"
+  args[2] = "output.txt" #output file
 }
 
 if(!require(dplyr)){
